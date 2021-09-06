15f40686e660d5bb02ed96a59abdd76393634dca
ext23 <xay345@gmail.com>
2021-09-06 18:07:31 +0300

    added react-final-form

--- src/components/TeamDetails.js
+++ src/components/TeamDetails.js
@@ -4,6 +4,7 @@ import { useGetTeamDetailsQuery } from '../api';
 import { useParams } from "react-router-dom";
 import { useStyles } from '../style'
 import { TextField } from '@material-ui/core';
+import { Form, Field } from 'react-final-form';
 
  
 const TeamDetails = (props) => {
@@ -12,28 +13,16 @@ const TeamDetails = (props) => {
 
     const { data, error, isLoading } = useGetTeamDetailsQuery({ matchId: props.matchId, teamId: teamId });
 
+    const clickOnSubmit = (e) => {
+        console.log("submit");
+    };
+
     return (
-        <div>
-            <h1>Информация о команде</h1>
-            {error ? (
-                <>error</>
-            ) : isLoading ? (
-                <>Загрузка...</>
-            ) : data ? (
-                <form>
-                    <div>
-                    <TextField label="Код" variant='filled' defaultValue={data.teams.id}></TextField>
-                    <TextField label="Название" variant='filled' defaultValue={data.teams.name}></TextField>
-                    </div>
-                    <div>    
-                    <TextField label="Капитан" variant='filled' defaultValue={data.teams.captainName}></TextField>                                   
-                    <div>Капитан: </div>
-                    <div>Помощник: {data.teams.assistantName}</div>
-                    </div>            
-                </form>                
-            ) : null
-            }
-        </div>
+        <Form onSubmit={onSubmit}>
+            <form onSubmit={clickOnSubmit}>
+            <h1>Информация о команде</h1>           
+            </form>
+        </Form>
     );
 }
 
