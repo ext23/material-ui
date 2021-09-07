c0a6dd6fc6a368963e153a8c80f95ec3169c8f17
ext23 <xay345@gmail.com>
2021-09-07 17:51:56 +0300

    Added mui-rff to TeamDetails form

--- src/components/TeamDetails.js
+++ src/components/TeamDetails.js
@@ -2,27 +2,71 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { useGetTeamDetailsQuery } from '../api';
 import { useParams } from "react-router-dom";
-import { useStyles } from '../style'
-import { TextField } from '@material-ui/core';
+import { makeStyles, Button, ButtonGroup } from '@material-ui/core';
 import { Form, Field } from 'react-final-form';
+import { Paper } from '@material-ui/core';
+import { TextField } from 'mui-rff'
 
- 
+const useStyles = makeStyles((theme) => ({
+    fields: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        '& > *': {
+            margin: theme.spacing(1)
+        }
+    },
+    buttonGroup: {    
+        display: 'flex',        
+        alignItems: 'left',
+        '& > *': {
+            margin: theme.spacing(1),           
+            fontSize: 10,
+        }
+    },
+}));
+
 const TeamDetails = (props) => {
     const { teamId } = useParams();
+    const { data, error, isLoading } = useGetTeamDetailsQuery({ matchId: props.matchId, teamId: teamId });
     const classes = useStyles();
 
-    const { data, error, isLoading } = useGetTeamDetailsQuery({ matchId: props.matchId, teamId: teamId });
-
-    const clickOnSubmit = (e) => {
-        console.log("submit");
+    const onSubmit = (values) => {
+        console.log(values);
     };
 
     return (
-        <Form onSubmit={onSubmit}>
-            <form onSubmit={clickOnSubmit}>
-            <h1>Информация о команде</h1>           
-            </form>
-        </Form>
+        <div>
+            <h1>Информация о команде: {!data || data.teams.name}</h1>
+            <Form
+                onSubmit={onSubmit}
+                initialValues={!data || {
+                    ...data.teams,
+                }}
+                render={({ handleSubmit, pristine, form, submitting }) => (
+                    <form onSubmit={handleSubmit}>
+                        <ButtonGroup className={classes.buttonGroup} color="secondary" variant="outlined">
+                            <Button type="submit" disabled={submitting || pristine} >
+                                Сохранить
+                            </Button>
+                            <Button type="button" onClick={form.reset} disabled={submitting || pristine}>
+                                Сбросить
+                            </Button>
+                        </ButtonGroup>
+                        <Paper className={classes.fields}>
+                            <div>                                
+                                <TextField label="Код команды" name="id" required={true} />
+                                <TextField label="Название команды" name="name" required={true} />                                
+                                <TextField label="Имя капитана" name="captainName" required={true} />
+                                <TextField label="Имя помощника" name="assistantName" required={true} />
+                                <TextField label="Основной пин-код" name="pin" required={true} />
+                                <TextField label="Резервный пин-код" name="pin"/>
+                            </div>
+                        </Paper>
+                    </form>
+                )
+                }
+            ></Form>
+        </div>
     );
 }
 
